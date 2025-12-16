c41380f9b717bb904eb0bad5dbd11360503f9b7d
Austin <freeluck.v.i.p999@gmail.com>
2025-12-16 02:08:13 -0600

    added loop that can not be broken unless you type exit

--- todo/src/main.rs
+++ todo/src/main.rs
@@ -1,3 +1,21 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Type 'exit' to exit the program");
+    escape_program(); // loop that ends with 'exit' typed by user 
+
+}
+fn escape_program() {
+    loop {
+      let mut input = String::new();
+       io::stdin().read_line(&mut input).expect("can't read input");
+
+       //removing newline character
+       let escape = input.trim();
+
+       if escape == "exit" {
+            break;
+        }
+    }
+
 }
