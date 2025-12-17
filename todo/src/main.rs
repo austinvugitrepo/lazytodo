d6d644a01b1db6cd335996089a12df3f56807f3a
Austin <freeluck.v.i.p999@gmail.com>
2025-12-16 18:36:21 -0600

    added function to put user input into a file

--- todo/src/main.rs
+++ todo/src/main.rs
@@ -1,21 +1,32 @@
 use std::io;
+use std::io::Write;
+use std::fs::File;
 
-fn main() {
+fn main() -> std::io::Result<()> {
+    File::create_new("lazytodo.txt")?; // make todo file 
     println!("Type 'exit' to exit the program");
  loop {
     let mut input = String::new();
     io::stdin().read_line(&mut input).expect("can't read input");
        //removing newline character
-    let new_input = input.trim();
+    let mut new_input = input.trim();
         if new_input == "exit"{
-           break;
+           break Ok(());
        } else {
            println!("writing '{}' to todo list...", new_input);
+           let _ = iowrite(&mut new_input); //writes to lazytodo.txt
+           println!("sucessfully added '{}' to todo list...", new_input);
         }
    
        
 
   }
 
+}
+
+fn iowrite(inputstr: &mut &str) -> std::io::Result<()> {
+    let mut fappend = File::options().append(true).open("lazytodo.txt")?; //appending to file 
+    writeln!(&mut fappend, "{}", inputstr)?; // write to lazytodo File 
+    Ok(())
 
 }
